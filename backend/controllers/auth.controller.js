ecc1e2488d9a9b411cc645494c3592558518f3d3
Soumik Majumder <soumikcode02@gmail.com>
2025-01-09 00:36:35 +0530

    redis setup, authenticate user with access and refresh token, cookies

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,41 @@
 import userModel from "../lib/database/models/user.model.js"
+import jwt from "jsonwebtoken"
+import { redis } from "../lib/redis.js"
+
+function generateTokens(userId)
+{
+    const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "15m"
+    })
+
+    const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: "7d"
+    })
+
+    return { accessToken, refreshToken };
+}
+
+async function setRefreshToken(userId, refreshToken)
+{
+    await redis.set(`refresh_token:${userId}`, refreshToken, "EX", 7 * 24 * 60 * 60);
+}
+
+function setCookies(res, accessToken, refreshToken)
+{
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV != "development",
+        sameSite: "strict",
+        maxAge: 15 * 60 * 1000
+    })
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV != "development",
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    })
+}
 
 async function signup(req, res)
 {
@@ -17,8 +54,14 @@ async function signup(req, res)
         }
 
         const newUser = await userModel.create({ name, email, password });
+        
+        const { accessToken, refreshToken } = generateTokens(newUser._id);
+        
+        await setRefreshToken(newUser._id, refreshToken);
 
-        res.status(201).json({ message: "Signup successful" });
+        setCookies(res, accessToken, refreshToken);
+
+        res.status(201).json({ user: { _id: newUser._id, name: newUser.name, email: newUser.email, role: newUser.role }, message: "Signup successful" });
     }
     catch(err) {
         console.log("error in signup:", err.message);
