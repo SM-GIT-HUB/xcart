7e671a59c94d8e37822cb979e4fd0440bfa90192
Soumik Majumder <soumikcode02@gmail.com>
2025-01-27 00:35:24 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,15 @@ import cartRoutes from "./routes/cart.route.js"
 import couponRoutes from "./routes/coupon.route.js"
 import paymentRoutes from "./routes/payment.route.js"
 import analyticsRoutes from "./routes/analytics.route.js"
+import path from "path"
 
 import dbConnect from "./lib/database/db.js"
 import cookieParser from "cookie-parser"
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve()
 
 app.use(express.json());
 app.use(cookieParser());
@@ -26,6 +29,15 @@ app.use('/api/cart', cartRoutes);
 app.use('/api/coupons', couponRoutes);
 app.use('/api/payments', paymentRoutes);
 app.use('/api/analytics', analyticsRoutes);
+
+if (process.env.NODE_ENV != "development")
+{
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, async() => {
     await dbConnect();
